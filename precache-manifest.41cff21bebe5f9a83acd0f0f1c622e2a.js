b1a36ae80ee2e9d2c759edb4ed1b213a74d6cc68
Craig Derocher <craigderocher@Craig.local>
2019-03-11 21:35:25 -0500

    Updates

---
+++ precache-manifest.41cff21bebe5f9a83acd0f0f1c622e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3e9a5edfac54aee3190",
+    "url": "/clickygame_hw19/static/css/main.e72c8e28.chunk.css"
+  },
+  {
+    "revision": "b3e9a5edfac54aee3190",
+    "url": "/clickygame_hw19/static/js/main.0bd7b4c0.chunk.js"
+  },
+  {
+    "revision": "b2371f3f6a5b514bfb5f",
+    "url": "/clickygame_hw19/static/js/runtime~main.d975fcd6.js"
+  },
+  {
+    "revision": "a252e227d3244d802167",
+    "url": "/clickygame_hw19/static/js/2.31a730b4.chunk.js"
+  },
+  {
+    "revision": "72d372c3cbccd1de2c9a6bc4305fb546",
+    "url": "/clickygame_hw19/static/media/winner.72d372c3.jpg"
+  },
+  {
+    "revision": "a46a2cd83ec3c9d09fca8f5f1795cc29",
+    "url": "/clickygame_hw19/index.html"
+  }
+];
