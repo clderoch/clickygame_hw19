6fde71105451e523f62c744a975acbce52b24b6f
Craig Derocher <craigderocher@Craig.local>
2019-03-11 21:37:58 -0500

    message

--- clickygame/src/components/nav/nav.js
+++ clickygame/src/components/nav/nav.js
@@ -7,7 +7,7 @@ const Nav = () => (
         <a className="navbar-brand" href="/">Click-Out!!</a>
       </div>
       <ul className="nav navbar-nav">
-        <a href="https://github.com/mccaffertycr/clicky-game">GitHub Repo</a>
+        <a href="https://github.com/clderoch/clickygame_hw19">GitHub Repo</a>
       </ul>
     </nav>
 );
